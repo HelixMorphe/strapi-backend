06943eb5d2e3d1650b51c130c99afab471a243f5
santhosh-cloud <coe19b041@iiitdm.ac.in>
2021-08-23 00:33:23 +0530

    my first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0717d1a924efef41f93b97bc739cc6b8'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7e247fc4-334a-4fb3-8c87-e484ad641c71'
+};
